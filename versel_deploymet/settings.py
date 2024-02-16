e2a8b9d73a39dfda698c1080d86eff1eebf0b7af
adnan sadiq <adnansadiqxyz@gmail.com>
2024-02-16 10:37:07 +0500

    core origin added

--- versel_deploymet/settings.py
+++ versel_deploymet/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', '127.0.0.1:8080', '127.0.0.1:8081']
 
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8081",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,12 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'drf_yasg',
     'rest_framework.authtoken',
     'example'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
