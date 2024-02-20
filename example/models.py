6743ab41858db839dcdc84ed048dd95a0a314e86
adnan sadiq <adnansadiqxyz@gmail.com>
2024-02-20 22:44:53 +0500

    avater required removed

--- example/models.py
+++ example/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=20, choices=UserRole.choices, default=UserRole.ADMIN
     )
-    avatar = models.FileField(upload_to="static/users_avatars", blank=True)
+    avatar = models.FileField(upload_to="static/users_avatars", blank=True, null=True)
     phoneNumber = models.CharField(max_length=20, null=True, blank=True)
     first_name = None
     last_name = None
